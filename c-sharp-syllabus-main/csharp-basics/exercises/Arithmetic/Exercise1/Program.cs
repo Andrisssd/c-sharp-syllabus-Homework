f404babf284ccfaf92be097fe91f747a16f25f14
Andris <Andrisssd@gmail.com>
2021-11-24 15:28:37 +0200

    Done Exercise1

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            bool result = (firstNumber-secondNumber == 15 
+                || secondNumber - firstNumber == 15 
+                || firstNumber == 15 
+                || secondNumber == 15 
+                || firstNumber + secondNumber == 15);
+
+            Console.WriteLine(result);
+        }
+    }
+}
