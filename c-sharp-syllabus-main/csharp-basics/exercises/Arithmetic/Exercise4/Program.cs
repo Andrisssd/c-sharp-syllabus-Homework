e9c12f98c80792bbb6ed410189137ca23af90e7e
Andris <Andrisssd@gmail.com>
2021-11-24 15:50:02 +0200

    Done Exercise4

--- c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -6,7 +6,15 @@ namespace Exercise4
     {
         static void Main(string[] args)
         {
-            
+            int sum = 1;
+
+            for (int i = 1; i<=10; i++) {
+                sum*=i;
+            }
+            Console.WriteLine(sum);
+
+
+
         }
     }
 }
