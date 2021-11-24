27f1cf7848728d16ff8ea6fc1bfd3f6c2b4bc584
Andris <Andrisssd@gmail.com>
2021-11-24 11:41:55 +0200

    Done Arithmetics GravityCalculator

--- c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -11,7 +11,9 @@ namespace GravityCalculator
             double initialVelocity = 0.0;
             double fallingTime = 10.0;
             double initialPosition = 0.0;
-            double finalPosition = 0.0;
+            double finalPosition = (initialVelocity+(gravity*fallingTime))-initialPosition;
+
+
             Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
             Console.ReadKey();
         }
