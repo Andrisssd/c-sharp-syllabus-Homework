04716636bedac39f6b834ca4ac4f7f0613367be5
Andris <Andrisssd@gmail.com>
2021-11-24 15:42:22 +0200

    Done Exercise2

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Start:
+            Console.Write("Enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{ReturnNumOddEven(number)} number!");
+            QuitPoint:
+            Console.WriteLine("Quit? Y/n");
+            string quitChoice = Console.ReadLine().ToLower();
+
+            if (quitChoice=="y" || quitChoice=="n")
+            {
+                switch (quitChoice)
+                {
+                    case "y":
+                        Console.WriteLine("bye!");
+                        Environment.Exit(0);
+                        break;
+                    case "n":
+                        goto Start;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+                goto QuitPoint;
+            }
+            
+        }
+        public static string ReturnNumOddEven(int number)
+        {
+            if (number%2==0)
+            {
+                return "Even";
+            }
+            return "Odd";
+        }
+    }
+}
