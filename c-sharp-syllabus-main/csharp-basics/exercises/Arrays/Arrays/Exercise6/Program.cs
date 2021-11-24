cbb2b661327386441c2181ce1bb5752079e38e57
Andris <Andrisssd@gmail.com>
2021-11-24 18:14:53 +0200

    Done Exercise 6

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[10];
+            string str = "";
+            for(int i =0; i<array1.Length; i++)
+            {
+                array1[i] = new Random().Next(1, 101);
+            }
+
+            int[] array2 = new int[array1.Length];
+            Array.Copy(array1, array2, array1.Length);
+            array1[array1.Length-1] = -7;
+            str+="Array 1: ";
+            foreach(var num in array1)
+            {
+                str+=num+" ";
+            }
+
+            str+="\nArray 2: ";
+            foreach(var num in array2)
+            {
+                str+=num+" ";
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
