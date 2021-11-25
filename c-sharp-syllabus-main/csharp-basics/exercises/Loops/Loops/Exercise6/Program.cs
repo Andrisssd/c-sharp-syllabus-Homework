bdf08e5ce86570c8ecea81f5b6287cd6bc725e50
Andris <Andrisssd@gmail.com>
2021-11-25 16:09:29 +0200

    Done Loops Exercise 6

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Max value? ");
+            int maxValue;
+            int.TryParse(Console.ReadLine(), out maxValue);
+            string baseStr = "";
+            for(int i = 0; i < maxValue; i++)
+            {
+                if (i%15==0)
+                {
+                    baseStr += "FizzBuzz ";
+                }
+                else if(i%5==0)
+                {
+                    baseStr += "Buzz ";
+                }
+                else if (i%3==0)
+                {
+                    baseStr += "Fizz ";
+                }
+                else
+                {
+                    baseStr += i + " ";
+                }
+                if (i%20==0)
+                {
+                    baseStr += "\n";
+                }
+            }
+            Console.WriteLine(baseStr);
+        }
+    }
+}
