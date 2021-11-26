f04d4a3fbe300eea3c0aee92bf94689b2ab11e45
Andris <Andrisssd@gmail.com>
2021-11-26 16:47:48 +0200

    Done Exercise 5

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Date
+    {
+        public int year
+        {
+            get => date.Year;
+        }
+
+        public int month
+        {
+            get => date.Month;
+        }
+
+        public int day
+        {
+            get => date.Day;
+        }
+
+        public DateTime date;
+
+        public Date(int year, int month, int day)
+        {
+            date = new DateTime(year, month, day);
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"{year}/{month}/{day}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new Date(2000, 08, 01);
+            date.DisplayDate();
+            var date2 = new Date(2000, 45, 01);
+            date.DisplayDate();
+        }
+    }
+}
