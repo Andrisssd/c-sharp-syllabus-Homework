2cdf69c4a3c09dd58f54f9484b38abad197ffedd
Andris <Andrisssd@gmail.com>
2021-11-26 19:07:17 +0200

    Done VariablesAndNames exercise

--- c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
@@ -8,16 +8,13 @@ namespace VariablesAndNames
         {
             int cars, drivers, passengers, cars_not_driven, cars_driven;
             double seats_in_a_car, carpool_capacity, average_passengers_per_car;
-
-            cars = 100; //cars
-            seats_in_a_car = 4.0; //seats in a car
-            drivers = 28; // drivers
-            passengers = 90; // passengers
-            //cars_not_driven  // free cars
-            //cars_driven ; // cars driven at the moment
-            //carpool_capacity = // carpool capacity
-            //average_passengers_per_car = // average passengers per car
-
+            cars = 100;
+            seats_in_a_car = 4.0;
+            drivers = 28;
+            passengers = 90; 
+            cars_not_driven = cars - drivers;
+            carpool_capacity = drivers * seats_in_a_car;
+            average_passengers_per_car = passengers / drivers;
             Console.WriteLine("There are " + cars + " cars available.");
             Console.WriteLine("There are only " + drivers + " drivers available.");
             Console.WriteLine("There will be " + cars_not_driven + " empty cars today.");
