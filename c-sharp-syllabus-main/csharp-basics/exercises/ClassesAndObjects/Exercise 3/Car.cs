c5704b1dfdd1999a4196da91ee07fd0992ecf29d
Andris <Andrisssd@gmail.com>
2021-11-26 13:28:47 +0200

    Tried to do Exercise 3

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise 3/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    
+    class FuelGauge
+    {
+        private int liters;
+        
+        public void SetFuelLevel(int liters)
+        {
+            this.liters = liters;
+        }
+        public void IncrementFuelLevel()
+        {
+            if(liters < 70)
+            {
+                liters++;
+            }
+            else
+            {
+                Console.WriteLine("Max fuel");
+            }
+        }
+        public void DecrementFuelLevel()
+        {
+            if(liters >0)
+            {
+                liters--;
+            }
+            else
+            {
+                Console.WriteLine("None fuel left");
+            }
+        }
+
+        public void PrintGuelLevel()
+        {
+            Console.WriteLine($"{liters} L");
+        }
+
+    }
+    class Odometer
+    {
+        private int mileage;
+
+        public Odometer()
+        {
+            mileage = 0;
+        }
+        public void IncrementMileage()
+        {
+            mileage++;
+            if(mileage == 1)
+        }
+    }
+}
