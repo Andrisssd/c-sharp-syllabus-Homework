20d5202abf5f46a49ae67067026590637a490cd9
Andris <Andrisssd@gmail.com>
2021-11-26 11:39:07 +0200

    Done Exercise 1

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Product
+    {
+        private string name;
+        private double price;
+        private int amount;
+
+        public Product(string name, double price, int amount)
+        {
+            this.name = name;
+            this.price = price;
+            this.amount = amount;
+        }
+
+        public void SetNewPrice()
+        {
+            Console.Write($"Enter new price for {name}: ");
+            double newPrice = Convert.ToDouble(Console.ReadLine());
+            price = newPrice;
+        }
+
+        public void SetNewAmount()
+        {
+            Console.Write($"Enter new amount for {name}: ");
+            int newAmount = Convert.ToInt32(Console.ReadLine());
+            amount = newAmount;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{name}, price {price}, amount {amount}");
+        }
+    }
+}
