0a0f03616553b4ef35188ec7e34746763b38180b
Andris <Andrisssd@gmail.com>
2021-11-26 07:20:31 +0200

    Done Account Exercise 12

--- c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -10,6 +10,25 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            Account bartosAccount = new Account("Barto's account", 100.00);
+            Account bartosSwissAccount = new Account("Barto's account in Switzerland", 1000000.00);
+            Console.WriteLine("Initial state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);
+            bartosAccount.Withdrawal(20);
+            Console.WriteLine("Barto's account balance is now: "+bartosAccount.Balance());
+            bartosSwissAccount.Deposit(200);
+            Console.WriteLine("Barto's Swiss account balance is now: "+bartosSwissAccount.Balance());
+            Console.WriteLine("Final state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);
+            Console.WriteLine("=======================================");
+            Account mattsAccount = new Account("Matt's account", 1000);
+            Account myAccaunt = new Account("My poor account", 0);
+            mattsAccount.Withdrawal(100);
+            myAccaunt.Deposit(100);
+            Console.WriteLine(mattsAccount.ToString());
+            Console.WriteLine(myAccaunt.ToString());
         }
     }
 }
