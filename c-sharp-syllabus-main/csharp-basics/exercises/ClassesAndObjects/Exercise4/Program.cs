a2047be8bbb879b68ab5376c1b285866d28c21f2
Andris <Andrisssd@gmail.com>
2021-11-26 14:21:48 +0200

    Done Exercise 4

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Movie
+    {
+        private string _title;
+        private string _studio;
+        private string _rating;
+
+        public Movie(string Title, string Studio, string Rating)
+        {
+            this.Title = Title;
+            this.Studio = Studio;
+            this.Rating = Rating;
+        }
+
+        public Movie(string Title, string Studio)
+        {
+            this.Title = Title;
+            this.Studio = Studio;
+            Rating = "PG";
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Studio
+        {
+            get => _studio;
+            set => _studio = value;
+        }
+
+        public string Rating
+        {
+            get => _rating;
+            set => _rating = value;
+        }
+
+        public static Movie[] GetPG(Movie[] array)
+        {
+            int index = default;
+            int newArrayLength = default;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Rating.ToLower() == "pg")
+                {
+                    newArrayLength++;
+                }
+            }
+
+            Movie[] newArray = new Movie[newArrayLength];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Rating.ToLower() == "pg")
+                {
+                    newArray[index] = array[i];
+                    index++;
+                }
+            }
+
+            return newArray;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie first = new Movie("Casino Royale", "Eon Productions","PG13");
+            Movie second = new Movie("Glass", "Buena Vista International","PG13");
+            Movie third = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures","PG");
+            Movie[] movies = { first, second, third };
+            Movie[] filteredMovies = Movie.GetPG(movies);
+            foreach(var movie in filteredMovies)
+            {
+                Console.WriteLine($"{movie.Title} || {movie.Studio} || {movie.Rating}");
+            }
+        }
+    }
+}
