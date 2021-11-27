d92ba1defd96762a4624fbcbb5b3b09890dafed1
Andris <Andrisssd@gmail.com>
2021-11-27 09:43:34 +0200

    Done Exercise 7

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter something please: ");
+            string str = Console.ReadLine();
+            int sumOfUppercase = default;
+            for (int i = 0; i <str.Length; i++)
+            {
+                if (Char.IsUpper(str[i])){
+                    sumOfUppercase++;
+                }
+            }
+
+            Console.WriteLine($"Sum of Uppercase letters: {sumOfUppercase}");
+        }
+    }
+}
