f2dec459a3b423980a2d4323be1c97e231d6129e
Andris <Andrisssd@gmail.com>
2021-11-27 13:51:24 +0200

    Done Exercise 9

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace Exercise9
+{
+    class Program
+    {
+        public static void SwapPoints(ref Point firstPoint,ref Point secondPoint)
+        {
+            int bufferX, bufferY;
+            bufferX = firstPoint.X;
+            bufferY = firstPoint.Y;
+            firstPoint.X = secondPoint.X;
+            firstPoint.Y = secondPoint.Y;
+            secondPoint.X = bufferX;
+            secondPoint.Y = bufferY;
+
+        }
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(3, 4);
+            Point point2 = new Point(-3, 6);
+            SwapPoints(ref point1,ref point2);
+            Console.WriteLine($"({point1.X},{point1.Y})");
+            Console.WriteLine($"({point2.X},{point2.Y})");
+        }
+    }
+}
