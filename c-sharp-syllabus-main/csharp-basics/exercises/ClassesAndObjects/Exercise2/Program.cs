c1ab1381660a7d34549839aa67a1555e3478a919
Andris <Andrisssd@gmail.com>
2021-11-27 16:27:10 +0200

    Started Exercise 2

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Car
+    {
+        private double startKilometers;
+        private double endKulimeters;
+        double liters;
+        public Car(double startOdo, double endingOdo, double liters)
+        {
+            startKilometers = startOdo;
+            endKulimeters = endingOdo;
+            this.liters = liters;
+        }
+
+        public double CalculateConsumption()
+        {
+            endKulimeters - startKilometers
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
