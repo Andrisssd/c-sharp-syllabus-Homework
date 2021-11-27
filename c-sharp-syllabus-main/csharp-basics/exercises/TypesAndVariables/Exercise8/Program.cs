5e7d24e7151b93f90da354e4f924b6d9291af05a
Andris <Andrisssd@gmail.com>
2021-11-27 09:57:04 +0200

    Done Exercise 8

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of minutes: ");
+            long numOfMins = long.Parse(Console.ReadLine());
+            int days = (int)Math.Round(Convert.ToDecimal(numOfMins/60/24));
+            int years =(int)Math.Round(Convert.ToDecimal(numOfMins/525600));
+            Console.WriteLine($"Years:{years} || Days: {days%365}");
+        }
+    }
+}
