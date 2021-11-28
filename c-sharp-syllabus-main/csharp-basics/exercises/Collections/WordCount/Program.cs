c5a59cdaba25408dfdc79dea8acfed95911f4ccf
Andris <Andrisssd@gmail.com>
2021-11-28 18:42:06 +0200

    Done WordCount

--- c-sharp-syllabus-main/csharp-basics/exercises/Collections/WordCount/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using Fare;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using WebSupergoo.ABCpdf;
 
 namespace WordCount
 {
     class Program
     {
+        private const string TEXT_PATH = @"C:\Users\User\Desktop\homework\c-sharp-syllabus-Homework\c-sharp-syllabus-main\csharp-basics\exercises\Collections\WordCount\lear.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (File.Exists(TEXT_PATH))
+            {
+                string text = File.ReadAllText(TEXT_PATH);
+                string[] textLines = File.ReadAllLines(TEXT_PATH);
+
+                int numOfChars = textLines.Sum(s => s.Length);
+                int numOfLines = textLines.Length;
+                int numOfWords = Regex.Matches(text, @"\b\w+\b").Count;
+
+                Console.WriteLine("Lines = {0}",numOfLines);
+                Console.WriteLine("Words = {0}",numOfWords);
+                Console.WriteLine("Chars = {0}",numOfChars);
+            }
         }
     }
 }
