ec7f367fe8e15c1d02f77b5e709af658fe5502d3
Andris <Andrisssd@gmail.com>
2021-11-28 17:15:26 +0200

    Done Exercise 3

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/Exercise 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            StartAsking(names);
+        }
+        public static void StartAsking(HashSet<string> names)
+        {
+            Console.Write("Enter name:");
+            string input = Console.ReadLine();
+            while(input != "")
+            {
+                names.Add(input);
+                StartAsking(names);
+            }
+            string uniqueNames = "";
+            foreach(var name in names)
+            {
+                uniqueNames += name + " ";
+            }
+            Console.WriteLine($"Unique name list contains :{uniqueNames}");
+            Environment.Exit(0);
+        }
+    }
+}
