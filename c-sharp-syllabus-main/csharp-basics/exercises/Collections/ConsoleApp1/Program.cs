7767c0cc8d07d611febeb3a4c31d204caa822f02
Andris <Andrisssd@gmail.com>
2021-12-01 21:28:59 +0200

    Some Corrects

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+        string str = "";
+        for (int i = 0; i<=n; i++)
+        {
+            for (int j = 0; j<i; j++)
+            {
+                str+=n.ToString();
+            }
+
+            str+="\n";
+        }
+        Console.WriteLine(str);
+
+
+
+
+    }
+}
