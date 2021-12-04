73f3d423c0c7e421194d225e05c3ad3fbf0f647a
Andris <Andrisssd@gmail.com>
2021-12-04 08:56:11 +0200

    Done Exercise 3

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Polymorphism/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercise3
+{
+    public class Student : Person
+    {
+        private double _GPA;
+        public Student(string firstName, string lastName, string adress, int id, double GPA): base(firstName, lastName, adress, id)
+        {
+            _GPA = GPA; 
+        }
+
+        public double GPA { get => _GPA; set => value = _GPA; }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Current status: Student, GPA: {GPA}");
+        }
+    }
+
+    public class Employee : Person
+    {
+        private string _jobTitle;
+        public Employee(string firstName, string lastName, string adress, int id, string jobTitle) : base(firstName, lastName, adress, id)
+        {
+            _jobTitle = jobTitle;
+        }
+
+        public string JobTitle { get => _jobTitle; set => value = _jobTitle; }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Current status: Employee, Title: {JobTitle}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("*****", "#########", "@@@@@@@", 123123123, 9.8);
+            Employee employee = new Employee("*****", "#########", "@@@@@@@", 123123123, "Manager");
+            student.Display();
+            employee.Display();
+        }
+    }
+}
