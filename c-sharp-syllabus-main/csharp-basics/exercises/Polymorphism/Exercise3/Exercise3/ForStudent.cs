fbad6d46d8c5976c0a6bcd8d2e80fb27b4282cae
Andris <Andrisssd@gmail.com>
2021-12-08 08:12:24 +0200

    Corrected Exercise 3

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Polymorphism/Exercise3/Exercise3/ForStudent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    public class Student : Person
+    {
+        private double _GPA;
+        public Student(string firstName, string lastName, string adress, int id, double GPA) : base(firstName, lastName, adress, id)
+        {
+            _GPA = GPA;
+        }
+
+        public double GPA { get => _GPA; set => value = _GPA; }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Current status: Student, GPA: {GPA}");
+        }
+    }
+
+}
