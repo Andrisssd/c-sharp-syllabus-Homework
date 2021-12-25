249f5e47f2c792c7806bbb367172278c9f5af070
Andris <Andrisssd@gmail.com>
2021-12-25 21:34:41 +0200

    Method fixes

--- c-sharp-syllabus-main/csharp-basics/exercises/ScooterRentApp/If-Scooters_ver2_Tests/ScooterServiceTests.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/ScooterRentApp/If-Scooters_ver2_Tests/ScooterServiceTests.cs
@@ -6,18 +6,23 @@ namespace If_Scooters_ver2_Tests
     [TestClass]
     public class ScooterServiceTests
     {
+        private IScooterService _service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _service = new ScooterService();
+        }
+
         [TestMethod]
         public void AddScooter_OneScooter_OneScooterAdded()
         {
-            //Arrange
-            //Make new ScooterService object
-            IScooterService scooterService = new ScooterService();
             //Act
             //Add one scooter to scooterService's scooterList
-            scooterService.AddScooter("id", 0.05M);
+            _service.AddScooter("id", 0.05M);
             //Expect scooterList length equals 1
             int expectedScooterListLength = 1;
-            var actualScooterListLength = scooterService.GetScooters().Count;
+            var actualScooterListLength = _service.GetScooters().Count;
             //Assert
             //Compare expected scooterList length and actual
             Assert.AreEqual(expectedScooterListLength, actualScooterListLength);
@@ -26,28 +31,22 @@ namespace If_Scooters_ver2_Tests
         [TestMethod]
         public void Scooter_WhenMakingTwoScootersWithOneId_ShouldThrowException()
         {
-            //Arrange
-            //Initialize new IScooterService object
-            IScooterService scooterService = new ScooterService();
             //Act
             //Add one Scooter to ScooterService's scooterList
-            scooterService.AddScooter("id", 0.05M);
+            _service.AddScooter("id", 0.05M);
             //Assert
             //Check if adding new Scooter with same id throws an Expection
-            Assert.ThrowsException<AddScooterMethodException>(() => scooterService.AddScooter("id", 0.05M));
+            Assert.ThrowsException<AddScooterMethodException>(() => _service.AddScooter("id", 0.05M));
         }
 
         [TestMethod]
         public void GetScooterById_Id_ScooterWithThisId()
         {
-            //Arrange
-            //Make new ScooterService object
-            IScooterService scooterService = new ScooterService();
             //Act
             //Add one scooter to ScooterService's scooterList
-            scooterService.AddScooter("id1", 0.05M);
+            _service.AddScooter("id1", 0.05M);
             //Get scooter by id
-            Scooter actualScooter = scooterService.GetScooterById("id1");
+            Scooter actualScooter = _service.GetScooterById("id1");
             //Set expected scooter's id and actual scooter's id
             string expectedScootersId = "id1";
             string actualScootersId = actualScooter.Id;
@@ -59,15 +58,12 @@ namespace If_Scooters_ver2_Tests
         [TestMethod]
         public void GetScooters_ScooterList()
         {
-            //Arrange
-            //Make new ScooterService object
-            IScooterService scooterService = new ScooterService();
             //Act
             //Add one scooter to ScooterService's scooterList
-            scooterService.AddScooter("id", 0.05M);
+            _service.AddScooter("id", 0.05M);
             //Set expected scooters'id and first scooterList's elements id
             var expectedScootersId = new Scooter("id", 0.05M).Id;
-            var actualScootersId = scooterService.GetScooters()[0].Id;
+            var actualScootersId = _service.GetScooters()[0].Id;
             //Assert
             //Compare expected id and actual id
             Assert.AreEqual(expectedScootersId, actualScootersId);
@@ -76,16 +72,13 @@ namespace If_Scooters_ver2_Tests
         [TestMethod]
         public void RemoveScooter_OneScooter_EmptryScooterList()
         {
-            //Arrange
-            //Make new ScooterList object
-            IScooterService scooterService = new ScooterService();
             //Act
             //Add and remove one scooter
-            scooterService.AddScooter("id", 0.05M);
-            scooterService.RemoveScooter("id");
+            _service.AddScooter("id", 0.05M);
+            _service.RemoveScooter("id");
             //Set expected scooterList length and actual 
             int expectedScooterListLength = 0;
-            int actualScooterListLength = scooterService.GetScooters().Count;
+            int actualScooterListLength = _service.GetScooters().Count;
             //Assert
             //Compare scooterList length's
             Assert.AreEqual(expectedScooterListLength, actualScooterListLength);
@@ -94,19 +87,15 @@ namespace If_Scooters_ver2_Tests
         [TestMethod]
         public void GetScooterById_FakeId_ShouldThrowScooterNotFoundException()
         {
-            //Arrange
-            IScooterService service = new ScooterService();
             //Assert
-            Assert.ThrowsException<ScooterNotFoundException>(() => service.GetScooterById("Fake id"));
+            Assert.ThrowsException<ScooterNotFoundException>(() => _service.GetScooterById("Fake id"));
         }
 
         [TestMethod]
         public void RemoveScooter_FakeId_ShouldThrowScooterNotFoundException()
         {
-            //Arrange
-            IScooterService service = new ScooterService();
             //Assert
-            Assert.ThrowsException<ScooterNotFoundException>(() => service.RemoveScooter("Fake id"));
+            Assert.ThrowsException<ScooterNotFoundException>(() => _service.RemoveScooter("Fake id"));
         }
     }
 }
--- c-sharp-syllabus-main/csharp-basics/exercises/ScooterRentApp/If-Scooters_ver2_Tests/ScooterTests.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/ScooterRentApp/If-Scooters_ver2_Tests/ScooterTests.cs
@@ -6,6 +6,14 @@ namespace If_Scooters_ver2_Tests
     [TestClass]
     public class UnitTest1
     {
+        private Scooter _scooter;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _scooter = new Scooter("id", 0.05M);
+        }
+
         [TestMethod]
         public void Scooter_WhenRentPricePerMinuteIsLessThanZero_ShouldThrowInvalidPriceException()
         {
@@ -19,24 +27,18 @@ namespace If_Scooters_ver2_Tests
         [TestMethod]
         public void GetInvokeStartRentTime_DontInvokeStartRent_ShouldThrowInvokeStartRentTimeException()
         {
-            //Arrange
-            //Initialize new Scooter
-            Scooter scooter = new Scooter("id", 0.05M);
             //Assert
             //Check if GetInvokeStartRentTime throws an Exception 
-            Assert.ThrowsException<InvokeStartRentTimeException>(() => scooter.GetInvokeStartRentTime());
+            Assert.ThrowsException<InvokeStartRentTimeException>(() => _scooter.GetInvokeStartRentTime());
         }
 
         [TestMethod]
         public void ScooterConstructor_MakeOneScooter_ScootersIdAndPricePerMinute()
         {
-            //Arrange
-            //Make one scooter
-            Scooter scooter = new Scooter("id", 0.05M);
             //Act
             //Expected should be string representing scooter's id and price separated by space
             string exptected = "id 0.05";
-            string actual = $"{scooter.Id} {scooter.PricePerMinute}";
+            string actual = $"{_scooter.Id} {_scooter.PricePerMinute}";
             //Assert
             //Check if expected variable is equal to actual 
             Assert.AreEqual(exptected, actual);
