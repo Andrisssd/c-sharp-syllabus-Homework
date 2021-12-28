013029f5c8462307ea99f97cc8da1d5309b5a95f
Andris <Andrisssd@gmail.com>
2021-12-29 01:09:00 +0200

    Partly Done Exercise 6 Tests

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/ArithmeticsTests/Exercise6.Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using CozaLozaWoza_Exercise6;
+
+namespace ArithmeticsTests
+{
+    class Exercise6_Tests
+    {
+        private CozaLozaWoza _core;
+
+        [SetUp]
+        public void Setup()
+        {
+            _core = new CozaLozaWoza();
+        }
+
+        [Test]
+        public void GetCozaLozaWozaOrNumber_NumberThatDividesByFifteen_ReturnCozaLoza()
+        {
+            //Assign
+            int number = 15;
+            string expectedResult = "CozaLoza ";
+            //Assert
+            Assert.AreEqual(expectedResult, _core.GetCozaLozaWozaOrNumber(number));
+        }
+
+        [Test]
+        public void GetCozaLozaWozaOrNumber_NumberThatDividesBySeven_ReturnWoza()
+        {
+            //Assign
+            int number = 7;
+            string expectedResult = "Woza ";
+            //Assert
+            Assert.AreEqual(expectedResult, _core.GetCozaLozaWozaOrNumber(number));
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/CozaLozaWoza Exercise6/CozaLozaWoza.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozaLozaWoza_Exercise6
+{
+    public class CozaLozaWoza
+    {
+        public string GetCozaLozaWozaOrNumber(int i)
+        {
+            if (i%11==1)
+            {
+                return $"\n{i} ";
+            }
+            if (i%15==0)
+            {
+                return "CozaLoza ";
+            }
+            else if (i%7==0)
+            {
+                return "Woza ";
+            }
+            else if (i%5==0)
+            {
+                return "Loza ";
+            }
+            else if (i%3==0)
+            {
+                return "Coza ";
+            }
+
+            return i+" ";
+        }
+    }
+}
