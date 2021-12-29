42794abd3879fbb673f6f027a862280b3c138d5d
Andris <Andrisssd@gmail.com>
2021-12-29 15:21:13 +0200

    Done Exercise8 Tests

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Foo Corporation Exercise8/EmployeesPaymentCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foo_Corporation_Exercise8
+{
+    public class EmployeesPaymentCalculator
+    {
+        public decimal GetFooEmployeesPaymentForHours(decimal oneHourPayment, int hourstAWeek)
+        {
+            if (oneHourPayment >= 8M)
+            {
+                if (hourstAWeek <= 60)
+                {
+                    if (hourstAWeek <= 40)
+                    {
+                        return oneHourPayment * hourstAWeek;
+                    }
+                    else
+                    {
+                        return oneHourPayment * 40 + (hourstAWeek - 40) * oneHourPayment * 1.5M;
+                    }
+                }
+
+                throw new WeekOverworkException();
+            }
+
+            throw new EmployeesPaymentLimitException(oneHourPayment);
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Foo Corporation Exercise8/Exceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foo_Corporation_Exercise8
+{
+    [Serializable]
+    public class WeekOverworkException : Exception
+    {
+        public WeekOverworkException() { }
+
+        public WeekOverworkException(int hoursWorked) : base (String.Format("Employees total work hours per week shouldn't be negative and more than 65!"))
+        {
+
+        }
+    }
+
+    [Serializable]
+    public class EmployeesPaymentLimitException : Exception
+    {
+        public EmployeesPaymentLimitException() { }
+
+        public EmployeesPaymentLimitException(decimal oneHourPayment) : base (String.Format("{0} $/h is less than 8 $/h!",oneHourPayment)) 
+        {
+
+        }
+    }
+}
