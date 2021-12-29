1e43215138c15919e59274aad382029678a7be2b
Andris <Andrisssd@gmail.com>
2021-12-29 14:37:38 +0200

    Done Exercise7 Tests

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/ArithmeticsTests/Exercise7.Tests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using GravityCalculator_7;
+
+namespace ArithmeticsTests
+{
+    class Exercise7_Tests
+    {
+        private GravityCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new GravityCalculator();
+        }
+
+        [Test]
+        public void CalculateFinalPositionAfter_10Seconds_Return481Dot1805()
+        {
+            //Assign
+            double fallingTime = 10;
+            double expectedFinalPosition = 481.1805;
+            //Assert
+            Assert.AreEqual(expectedFinalPosition, _calculator.CalculateFinalPositionAfter(fallingTime));
+        }
+
+        [Test]
+        public void CalculateFinalPositionAfter_NegativeSeconds_ShouldThrowException()
+        {
+            //Assign
+            double fallingTime = -1;
+            //Assert
+            Assert.Throws<InvalidInputTimeException>(() => _calculator.CalculateFinalPositionAfter(fallingTime));
+        }
+
+        [Test]
+        public void GetResultString_ReturnResultString()
+        {
+            //Assign
+            string expectedResult = "The object's position after 10 seconds is 481.1805 m.";
+            //Act
+            _calculator.CalculateFinalPositionAfter(10);
+            string actualResult = _calculator.GetResultString();
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/GravityCalculator/GravityCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GravityCalculator_7
+{
+    public class GravityCalculator
+    {
+        private const double _gravity = -9.81;
+        private double _initialVelocity;
+        private double _initialPosition;
+        private double _fallingTime;
+        private double _finalPosition;
+
+        public GravityCalculator()
+        {
+            _initialVelocity = 0.0;
+            _initialPosition = 0.0;
+        }
+
+        public double CalculateFinalPositionAfter(double fallingTime)
+        {
+            if (fallingTime >= 0)
+            {
+                _fallingTime = fallingTime;
+                double finalPosition = (double)Math.Round((decimal)((0.5 * Math.Pow((_gravity*fallingTime), 2) + _initialVelocity*fallingTime + _initialPosition)/fallingTime), 4);
+                _finalPosition = finalPosition;
+                return finalPosition;
+            }
+
+            throw new InvalidInputTimeException(fallingTime);
+        }
+
+        public string GetResultString()
+        {
+            return $"The object's position after " + _fallingTime + " seconds is " + _finalPosition + " m.";
+        }
+    }
+}
