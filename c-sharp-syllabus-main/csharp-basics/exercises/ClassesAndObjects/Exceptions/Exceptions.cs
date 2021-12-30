955f5628a2c2637cb572895a7b05ef95bdab050e
Andris <Andrisssd@gmail.com>
2021-12-30 12:21:00 +0200

    Done Exercise1 Tests

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exceptions/Exceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exceptions
+{
+    [Serializable]
+    public class PriceIsLowerThanZeroException : Exception
+    {
+        public PriceIsLowerThanZeroException(double price) : base (String.Format("Price can't be less than zero. {0} < 0!",price))
+        {
+
+        }
+    }
+
+    [Serializable]
+    public class AmountIsLowerThanZeroException : Exception
+    {
+        public AmountIsLowerThanZeroException(int amount) : base(String.Format("Amount can't be less than zero. {0} < 0!", amount))
+        {
+
+        }
+    }
+}
