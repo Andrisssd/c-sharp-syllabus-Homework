0526058eb317f436acd5eff74c36b3b71203b3cb
Andris <Andrisssd@gmail.com>
2021-12-30 10:01:09 +0200

    Done Exercise6 Tests

--- c-sharp-syllabus-main/csharp-basics/exercises/Collections/FlightPlanner/Properties/AssemblyInfo.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/FlightPlanner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlightPlanner")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7617a60c-18a2-4ff5-9191-7137c7c2e8da")]
+[assembly: Guid("124264ee-f592-4cbd-9272-e2f64ff75114")]
 
 // Version information for an assembly consists of the following four values:
 //
