d8ff8243ed821ab1859894e02f8cfc9159ffff9f
Andris <Andrisssd@gmail.com>
2022-01-11 18:11:57 +0200

    Some LogInStatus changes

--- c-sharp-syllabus-main/csharp-basics/exercises/AteaHomework/AzureFunction/Models/LogInStatus.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/AteaHomework/AzureFunction/Models/LogInStatus.cs
@@ -5,31 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage.Table;
-using TableEntity = Microsoft.WindowsAzure.Storage.Table.TableEntity;
 using ITableEntity = Microsoft.WindowsAzure.Storage.Table.ITableEntity;
+using Microsoft.WindowsAzure.Storage;
 
 namespace Models
 {
     public class LogInStatus : ITableEntity
     {
-        public string GuidKey { get; set; }
-        public string Status => RowKey;
-        public string StatusCode { get; set; }
-        public string Time => PartitionKey;
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string ETag { get; set; }
 
-        public LogInStatus(string status, int statusCode)
+        public LogInStatus(string partitionKey, string rowKey, DateTimeOffset timestamp, string ETag)
         {
-            PartitionKey = DateTime.Now.ToString();
-            GuidKey = Guid.NewGuid().ToString();
-            RowKey = status;
-            StatusCode = statusCode.ToString();
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+            Timestamp = timestamp;
+            this.ETag = ETag;
         }
 
-        public LogInStatus() { }
-
-        public override string ToString()
+        public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
         {
-            return $"Status: {Status} Status code: {StatusCode} Request time: {Time} Guid: {GuidKey}";
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            throw new NotImplementedException();
         }
     }
 }
