6d7b92e49e27102e9de81f56ece77efabf2ee513
Andris <Andrisssd@gmail.com>
2022-01-17 21:15:46 +0200

    last fixes

--- c-sharp-syllabus-main/csharp-basics/exercises/Sudoku/Sudoku/Form1.Designer.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Sudoku/Sudoku/Form1.Designer.cs
@@ -1143,7 +1143,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(24, 920);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(630, 29);
+            this.button1.Size = new System.Drawing.Size(630, 71);
             this.button1.TabIndex = 100;
             this.button1.Text = "Delete selected save";
             this.button1.UseVisualStyleBackColor = true;
@@ -1154,7 +1154,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Sudoku.Properties.Resources.background;
-            this.ClientSize = new System.Drawing.Size(667, 953);
+            this.ClientSize = new System.Drawing.Size(667, 1003);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.bLoadSave);
